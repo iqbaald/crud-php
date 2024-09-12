7b73fc36edb0bdd4e45c7f2b71f15fae99b2b75a
Muhamad Iqbaal Dhoifulloh <iqbaal.dhoifulloh@gmail.com>
2024-09-12 16:13:23 +0700

    change background feature

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layout/template')
 @section('konten')
       <!-- START DATA -->
+
+        <div class="overlay"></div>
+
         <div class="my-3 p-3 bg-body rounded shadow-sm">
                 <!-- FORM PENCARIAN -->
                 <div class="pb-3">
@@ -52,5 +55,71 @@
                 {{$data->withQueryString()->links() }}
                
           </div>
+
+          <div id="btn-set-bg">
+            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-settings"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /></svg>
+        </div>
+        <div id="set-bg">
+            <div class="top-set-bg">
+                <span>Klik pada Gambar</span>
+                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+            </div>
+            <div class="main-set-bg">
+                <div id="img-1" class="img-set">
+                    <img src="{{asset('assets/thumb-forest.jpg')}}" alt="thumb-forest.jpg" data-bg="{{ asset('assets/ori-forest.jpg') }}">
+                </div>
+                <div id="img-2" class="img-set">
+                    <img src="{{asset('assets/thumb-sea.jpg')}}" alt="thumb-sea.jpg" data-bg="{{ asset('assets/ori-sea.jpg') }}">
+                </div>
+                <div id="img-3" class="img-set">
+                    <img src="{{asset('assets/thumb-forest.jpg')}}" alt="thumb-forest.jpg" data-bg="{{ asset('assets/ori-forest.jpg') }}">
+                </div>
+                <div id="img-4" class="img-set">
+                    <img src="{{asset('assets/thumb-sea.jpg')}}" alt="thumb-sea.jpg" data-bg="{{ asset('assets/ori-sea.jpg') }}">
+                </div>
+                <div id="img-5" class="img-set">
+                    <img src="{{asset('assets/thumb-sea.jpg')}}" alt="thumb-sea.jpg" data-bg="{{ asset('assets/ori-sea.jpg') }}">
+                </div>
+                <div id="img-6" class="img-set">
+                    <img src="{{asset('assets/thumb-sea.jpg')}}" alt="thumb-sea.jpg" data-bg="{{ asset('assets/ori-sea.jpg') }}">
+                </div>
+                <div id="img-7" class="img-set">
+                    <img src="{{asset('assets/thumb-sea.jpg')}}" alt="thumb-sea.jpg" data-bg="{{ asset('assets/ori-sea.jpg') }}">
+                </div>
+                <div id="img-8" class="img-set">
+                    <img src="{{asset('assets/thumb-sea.jpg')}}" alt="thumb-sea.jpg" data-bg="{{ asset('assets/ori-sea.jpg') }}">
+                </div>
+                <div id="img-9" class="img-set">
+                    <img src="{{asset('assets/thumb-sea.jpg')}}" alt="thumb-sea.jpg" data-bg="{{ asset('assets/ori-sea.jpg') }}">
+                </div>
+                <div id="img-4" class="img-set">
+                    <img src="{{asset('assets/thumb-sea.jpg')}}" alt="thumb-sea.jpg" data-bg="{{ asset('assets/ori-sea.jpg') }}">
+                </div>
+            </div>
+        </div>
           <!-- AKHIR DATA -->
+          <!-- SCRIPT SEMENTARA -->
+
+        <script>
+           const openMenu = document.getElementById('btn-set-bg');
+           const menu = document.getElementById('set-bg');
+           const images = document.querySelectorAll('.img-set img');
+           const closeMenu = document.querySelector('.top-set-bg svg');
+
+           openMenu.addEventListener('click', () => {
+            menu.style.display = 'block';
+           })
+
+           closeMenu.addEventListener('click', () =>{
+            menu.style.display= 'none';
+           })
+
+           images.forEach(image => {
+            image.addEventListener('click', () => {
+                const bgImage = image.getAttribute('data-bg');
+                document.body.style.backgroundImage = `url('${bgImage}')`;
+            })
+           })
+        </script>
+
 @endsection
